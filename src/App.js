9765112248df5471b672c9742372f62bacd2f315
Moises Ariza <arizamoises@gmail.com>
2022-07-21 18:54:00 -0500

    Modificación en tiempo real con los setvalues

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 function App() {
 
 
-  const [linea1, setLinea1] =  useState('holi');
-  const [linea2, setLinea2] =  useState('holi');
+  const [linea1, setLinea1] =  useState('');
+  const [linea2, setLinea2] =  useState('');
 
   const onChangeLinea1 = function(evento){
-    alert(evento.target.value)
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value)
   }
 
   return (
@@ -22,11 +26,12 @@ function App() {
         <option value="smart">Smart Guy</option>
       </select> <br />
       <input onChange={onChangeLinea1} type="text" placeholder='mensaje'/> <br />
-      <input type="text" placeholder='mensaje 2'/> <br />
+      <input onChange={onChangeLinea2} type="text" placeholder='mensaje 2'/> <br />
       <button>Exportart</button>
 
       <div>
         <span>{linea1}</span>
+        <br />
         <span>{linea2}</span>
       </div>
 
